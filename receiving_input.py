b4c8fd3b4bd71718715ac4dd257aacd2ebd8907e
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-16 16:50:33 +0530

    Update receiving_input.py

--- receiving_input.py
+++ receiving_input.py
@@ -1,1 +1,9 @@
+name=input('What is your name? ')   # "input()" is a built in function Which takes input from user, in the form of string. And the "name" is used to store the user entered data.
+print('Hi '+ name)                  # The term "Hi" and name are Combining/concatenating with the help of "+" symbol.
 
+
+
+#Write a program to ask two questions: Person's name and favourite color. Then print a message like "Dhanush likes Green".
+name=input("Please enter your name:-")
+color=input("Please enter your favourite colour:-")
+print(name+" likes "+color)
