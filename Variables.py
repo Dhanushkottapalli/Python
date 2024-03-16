3b35cc1d4c47ccc314991c87f2ee84f88ae6e4fb
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-16 16:25:46 +0530

    Update and rename Variables to Variables.py

---
+++ Variables.py
@@ -0,0 +1,12 @@
+price=10                      #If a product price is $10. Then $10 will be stored in in "Price" And if you want to acess the cost of a product then you can use "Price".
+price=20                      #Python intreptor will consider this value($20) as Price. Because python intreptor will reed code line by line.
+print(price)                  #If you run this program it will print the value stored in "Price".
+rating=4.9                    #In this line 4.9 is consider as "Floating point".
+name="Dhanush Kottapalli"     #In this line "Dhanush Kottapalli" is consider as "String" and it is stored in a variable called "name".
+is_publish=True               #In this line "is_publish" is consider as "Boolian value" and it will be indicated with "True" or "False" with first letter should be capital.
+
+
+# Write a Program to check in a patient named "Dhanush kothapalli". He's 20 years old and new patient.
+print("My name is Dhanush Kothapalli.")
+print("My age is 20 years.")
+print("Iam a new patient.")
