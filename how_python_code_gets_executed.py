476d2f327fc83f28e6d66e11eb6994276784ad8a
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-16 15:58:07 +0530

    Update and rename Concatenation.py to how_python_code_gets_executed.py

---
+++ how_python_code_gets_executed.py
@@ -0,0 +1,5 @@
+print("Hello world")
+print("o-----")         #What ever is written in "" it is defined as string.
+print(' ||||')          #Python code will be execuited line by line.
+
+print('*' * 10)         #We are multiplaying string.
