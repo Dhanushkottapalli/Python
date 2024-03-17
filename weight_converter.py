6e9d85785ba8563acaadff2461baa69fda605de8
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-18 01:21:05 +0530

    Update weight_converter.py

--- weight_converter.py
+++ weight_converter.py
@@ -1,1 +1,11 @@
-
+# weight_converter
+weight = int(input("Please enter your weight:-"))
+unit= input("(k/K)kg or (g/G)grams:-")
+if unit.upper() == 'K':
+    x = weight*1000
+    print(f"Your weight is {x}grams")
+elif unit.upper() == 'G':
+    y=weight/1000
+    print(f"Your weight is {y}Kg")
+else:
+    print("Thank you.")
