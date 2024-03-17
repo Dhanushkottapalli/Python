a78e11f0cde2ff1139963388c0058739c91e97dc
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-17 11:46:53 +0530

    Update operator_precedence.py

--- operator_precedence.py
+++ operator_precedence.py
@@ -1,1 +1,16 @@
+x=10+3*2  #First it will multiply 3*2 and then add to 10 the answer will be 16
+print(x)
+'''
+Exponentiation 2**3
+Multiplaction or Devision
+Addition or Subtraction
+'''
+#Exponentiation operator
+x=10+3*2**2  #1st python interpreter will do 2*2 then 4*3 then 10+12 the aswer is 22.
+print(x)
+#I can also use parenthesis to change the order of operations,If we have parenthesis it will take Priority.
+x=(10+3)*2**2#first it will add 10+3 then 4 is multiplied by 30.
+print(x)
 
+x=(2+3)*10-3#First it will solve 2+3 and then multiply with 10 and subtract with 3.
+print(x)
