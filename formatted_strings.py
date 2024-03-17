97c41dc71cd186267c40755fd1da027e734eaa27
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-17 09:39:03 +0530

    Update formatted_strings.py

--- formatted_strings.py
+++ formatted_strings.py
@@ -1,1 +1,13 @@
+#If you want to print "Dhanush [Kottapalli] is a coder"
+#We can do it in two types:-
+#String Concatenation.
+#Formatted Strings.
+first="Dhanush"
+last="Kottapalli"
+message = first+" ["+last+"] is a coder"   #String Concatenation.
+print(message)
+print(first+" ["+last+"] is a coder")      #String Concatenation.
 
+msg= f"{first}[{last}] is a coder"         #Formatted Strings.
+print(msg)
+print(f"{first}[{last}] is a coder")       #Formatted Strings.
