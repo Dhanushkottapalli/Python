a5b9d4fa2a5c94bd0844ed69a823e578743113ad
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-18 01:32:41 +0530

    Update while_loops.py

--- while_loops.py
+++ while_loops.py
@@ -1,1 +1,11 @@
+# while_loops
+i = 1
+while i <= 5:     #While loop will keeps on execute till this statement become false.
+    print(i)      #It will print the value stored in i.
+    i += 1        #this sentance will increament i with 1.after compliting this the walue of i will increamented by 1 and becomes 2.
 
+i = 1
+while i <= 5:     #While loop will keeps on execute till this statement become false.
+    print('*' * i)#It will print the value stored in i.
+    i += 1        #this sentance will increament i with 1.after compliting this the walue of i will increamented by 1 and becomes 2.
+print("Done")
