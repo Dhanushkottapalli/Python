a4b0e40630aed8a39d34a5bd8df8da975b95d19d
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-17 11:57:18 +0530

    Update math_functions.py

--- math_functions.py
+++ math_functions.py
@@ -1,1 +1,9 @@
+x=2.9  #if we want to round this number we have a function (riund()).
+print(round(x))
+x= -2.9  #if we want to change negitive number to positive number we have a function (abs()).
+print(abs(x))
 
+#if you want to write complex mathematical programs then we should inport math module.
+import math
+print(math.ceil(2.9))
+print(math.floor(2.9))
