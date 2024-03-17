cf89bf272c0186ee8033e7c198001b92a3181942
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-18 01:46:46 +0530

    Update gussing_game.py

--- gussing_game.py
+++ gussing_game.py
@@ -1,1 +1,12 @@
-
+# gussing_game.py
+secret_number = 9
+guss_count=0
+guss_limit=3
+while guss_count<guss_limit:
+    guess=int(input('Guss:-'))
+    guss_count +=1
+    if guess == secret_number:
+        print('You win!')
+        break
+else:
+    print("Try one more time")
