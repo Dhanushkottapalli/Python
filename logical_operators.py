45df9e8f1c68a48b31c254e8640e6c4e87eab7d8
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-17 17:13:56 +0530

    Update logical_operators.py

--- logical_operators.py
+++ logical_operators.py
@@ -1,1 +1,17 @@
+# Logical_Operator
+# if applicant has high income AND good credit Eligible for loan
+has_high_income=True
+has_high_credit=True
+if has_high_income and has_high_credit: #There are two condition, If we want to print out put when the two conditions are true then we will use "and" operator.
+    print("Eligible for loan")
 
+has_high_income=False
+has_high_credit=True
+if has_high_income or has_high_credit: #There are two condition, If we want to print out put when the one conditions are true then we will use "or" operator.
+    print("Eligible for loan")
+
+# if applicant has good credit And doesn't have a criminal record
+has_high_income=True
+has_criminal_record=True
+if has_high_income and not has_criminal_record: #And not operator is wsed to revers the boolian value.
+    print("Eligible for loan")
