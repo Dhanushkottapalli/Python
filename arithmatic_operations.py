96a032994047210dd2343cc79c8c0daa1c6b6b40
Dhanushkottapalli <151417733+Dhanushkottapalli@users.noreply.github.com>
2024-03-17 10:58:00 +0530

    Update arithmatic_operations.py

--- arithmatic_operations.py
+++ arithmatic_operations.py
@@ -1,1 +1,31 @@
+#Arithmetic operators
+10#Heare 10 is integer.
+10.01#Heare 10.01 is floting point number.
+print(10+3)  #This is an Addition operator.
+print(10-3)  #This is an Subtraction operator.
+print(10*3)  #This is an Multiplaction operator.
+#We have two kind of devision.
+10/3#This will give your answer in floting point.
+10//3#This will give your answer in integer.
+print(10/3)
+print(10//3)
+print(10%3)  #This is an Modules operator Which will return reminder.
+print(10**3)  #This is an Exponent operator Which will return sqare/cube of a number.
 
+#Argumanted assignment operator.
+
+x=10#If you want to increment 10 by 3
+x=x+3#We will take x=x+3 and value 13 will be stored in X
+print(x)#and print x, This is one type of incrementing a number.
+
+x=10
+x+=3 #This line exactly what is writen in line no:-18,(+=)This is what the Argumented assignment operator.
+print(x)
+
+x=10
+x-=3
+print(x)
+
+x=10
+x*=3
+print(x)
